b4451cbbcb7b66a9b2e4d3074d2723b4d534795e
anandaprlyaa <aprilyaananda6@gmail.com>
2021-04-23 16:17:52 +0700

    UserProject

---
+++ app/src/main/java/com/anandaprlyaa/userproject/MyAdapter.java
@@ -0,0 +1,63 @@
+package com.anandaprlyaa.userproject;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    private List<ListItem> listItems;
+    private Context context;
+
+    public MyAdapter(List<ListItem> listItems, Context context) {
+        this.listItems = listItems;
+        this.context = context;
+    }
+
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.List_item, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyAdapter.ViewHolder holder, int position) {
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        ListItem listItem = listItems.get(position);
+
+        holder.textViewHead.setText(listItem.getHead());
+        holder.textViewDesc.setText(listItem.getDesc());
+    }
+
+    }
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView textViewHead;
+        public TextView textViewDesc;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            textViewHead = (TextView) itemView.findViewById(R.id.textViewHead);
+            textViewDesc = (TextView) itemView.findViewById(R.id.textViewDesc);
+        }
+    }
+}
